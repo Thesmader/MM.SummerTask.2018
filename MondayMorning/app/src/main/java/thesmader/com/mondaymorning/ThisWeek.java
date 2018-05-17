1dcab502552e11e9ead5df97bbdfd8ed8257e9ec
Thesmader <dsmarak17@gmail.com>
2018-05-17 20:47:05 +0530

    Log In Screen Added
    ThisWeek activity is available in the code but has nothing except a hello world.
    
    Buttons are available, they don't lead anywhere.
    SignUp screen to be added soon.
    
    Signed-off-by: Thesmader <dsmarak17@gmail.com>

---
+++ MondayMorning/app/src/main/java/thesmader/com/mondaymorning/ThisWeek.java
@@ -0,0 +1,13 @@
+package thesmader.com.mondaymorning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThisWeek extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_this_week);
+    }
+}
