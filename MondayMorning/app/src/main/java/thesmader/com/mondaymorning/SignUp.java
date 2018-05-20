bf756188675b5a5f6cc851be7114f3dc5e4ce267
Thesmader <dsmarak17@gmail.com>
2018-05-21 01:09:04 +0530

    SignUp UI added.
    It is possible to navigate between the two activities using the login and signup buttons. SignUp is the launcher activity. New apk is available.
    
    Signed-off-by: Thesmader <dsmarak17@gmail.com>

--- MondayMorning/app/src/main/java/thesmader/com/mondaymorning/SignUp.java
+++ MondayMorning/app/src/main/java/thesmader/com/mondaymorning/SignUp.java
@@ -1,14 +1,14 @@
 package thesmader.com.mondaymorning;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.design.button.MaterialButton;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class Login extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     private MaterialButton signUpBtn,logInBtn;
 
@@ -16,21 +16,21 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //make activity fullscreen
+        //Make the activity fullscreen
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
 
         //button assignment
-        signUpBtn = findViewById(R.id.login_sup);
-        logInBtn = findViewById(R.id.login_lin);
+        signUpBtn = findViewById(R.id.signup_sup);
+        logInBtn = findViewById(R.id.signup_lgin);
 
-        //OnClickListeners for buttons
-        signUpBtn.setOnClickListener(new View.OnClickListener() {
+        //OnClickListeners for the buttons
+        logInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Login.this,SignUp.class));
+                startActivity(new Intent(SignUp.this,Login.class));
             }
         });
     }
